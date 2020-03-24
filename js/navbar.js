5233c764240741b7c6465d20ddf9fe029c6db18c
Koyoo898 <wojtek78890@o2.pl>
2020-03-24 14:39:08 +0100

    "add navbar and footer"

---
+++ js/navbar.js
@@ -0,0 +1,6 @@
+let nav = '<nav><a href="index.html">Home</a>'+
+    ' <a href="contact.html">Kontakt</>'+
+    ' <a href="quiz.html">Quiz</>';
+
+let navbarElem = document.querySelector(".navbar");
+navbarElem.innerHTML =nav;
